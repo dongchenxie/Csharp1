6d5279b08afeeeea41c639355622a2ebe00fd025
xdc811 <37362503+xdc811@users.noreply.github.com>
2019-10-30 10:01:04 -0700

    fsdfafsdsafsdf

---
+++ Csharp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Csharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = "Hello World";
+            String hi;
+            Console.WriteLine(message);
+        }
+    }
+}
