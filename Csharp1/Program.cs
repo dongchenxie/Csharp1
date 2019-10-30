cd7cf97c529d8c92408cfd445dbc644cd46b43aa
xdc811 <37362503+xdc811@users.noreply.github.com>
2019-10-30 11:03:08 -0700

    using eval

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DynamicExpresso;
+
 
 namespace Csharp1
 {
@@ -7,8 +9,17 @@ namespace Csharp1
         static void Main(string[] args)
         {
             String message = "Hello World";
-            String hi;
-            Console.WriteLine(message);
+            double implicitDouble = 123.45;
+            double explicitDouble = 123.45d;
+            float explicitFloat = 123.45f;
+            decimal explicitDecimal = 123.45m;
+            uint unsignedInt = 100u;
+            long longInteger = 100l;
+            ulong unsignedLongInteger = 100ul;
+            bool b = false;
+            var interpreter = new Interpreter();
+            var result = interpreter.Eval("8 /( 2 + 2)");
+            Console.WriteLine(result);
         }
     }
 }
